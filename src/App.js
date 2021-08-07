cbeb59ba9fa555822a063d0bfe285b6f94b7372c
Blackhawk95 <abhijith.newton@gmail.com>
2021-08-07 23:17:13 +0530

    :( failed to build in netlify

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
+//import logo from "./logo.svg"
 import pavlogo from "./vada_pav.webp"
 import "./App.css"
-
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
 
 class App extends Component {
   render() {
@@ -38,7 +12,6 @@ class App extends Component {
           <p>
             Vada Pav ?
           </p>
-          <LambdaDemo />
         </header>
       </div>
     )
